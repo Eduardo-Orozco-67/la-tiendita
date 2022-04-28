9cccc12ffd693080298a17fe9476b414fc12d2c0
Jose Eduardo Orozco Cardenas <eduardo.orozco66@unach.mx>
2022-04-27 23:17:22 -0500

    first commit

---
+++ src/main/java/Conecction/Conecction.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Conecction;
+
+/**
+ * 
+ * @author <Eduardo Orozco eduardo.orozco66@unach.mx>
+ */
+public class Conecction {
+
+}
